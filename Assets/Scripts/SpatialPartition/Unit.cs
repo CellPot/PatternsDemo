704e90ad7dcd276003418cfaa8176caa7007edc6
Dean Vice <anondean@mail.ru>
2021-02-03 22:08:25 +0300

    spatial partition init

---
+++ Assets/Scripts/SpatialPartition/Unit.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+   private Vector2 _initPosition;
+
+   private void Awake()
+   {
+      _initPosition = gameObject.transform.position;
+   }
+
+   public void Move(Vector2 positionDelta)
+   {
+      
+   }
+}
+
+public class Grid
+{
+   public int CellsNum = 10;
+   public int CellSize = 20;
+
+   private Unit[,] _cells;
+
+   public Grid()
+   {
+      _cells  = new Unit[CellsNum, CellsNum];
+   }
+   
+   public void Clear()
+   {
+      for (int i = 0; i < CellsNum; i++)
+      {
+         for (int j = 0; j < CellsNum; j++)
+         {
+            _cells[i, j] = null;
+         }
+      }
+   }
+}
